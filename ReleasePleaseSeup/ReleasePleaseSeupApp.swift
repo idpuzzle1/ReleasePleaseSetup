ed186a0c8a46a30e766e31bf04190fa063b75282
idpuzzle1 <idpuzzle1@gmail.com>
2023-06-05 12:12:46 +0300

    Initial Commit

---
+++ ReleasePleaseSeup/ReleasePleaseSeupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleasePleaseSeupApp.swift
+//  ReleasePleaseSeup
+//
+//  Created by Anton Ukhankin on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReleasePleaseSeupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
